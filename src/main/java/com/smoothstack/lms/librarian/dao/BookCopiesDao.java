5c96d364881aa8c4937748b31dbf69fd4b93881c
Allen-C-Chen <52220585+Allen-C-Chen@users.noreply.github.com>
2019-07-22 14:34:48 -0500

    Update BookCopiesDao.java

--- src/main/java/com/smoothstack/lms/librarian/dao/BookCopiesDao.java
+++ src/main/java/com/smoothstack/lms/librarian/dao/BookCopiesDao.java
@@ -11,8 +11,6 @@ import com.smoothstack.lms.librarian.model.BookCopiesID;
 
 public interface BookCopiesDao extends JpaRepository<BookCopies, BookCopiesID>{
 	
-	//@Query(value = "SELECT * FROM tbl_book_copies WHERE branchId = ?1", nativeQuery = true)
     public List<BookCopies> findByid_libraryBranch_branchID(Integer branchID);
-	//public Optional<List<BookCopies>> findByBranchId(Integer branchId);
 
 }
